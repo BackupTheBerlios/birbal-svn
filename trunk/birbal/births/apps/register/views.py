f6dfe9647840a2f6d4679422529390fd35716007
lawgon <lawgon@d3dd426a-df03-0410-81c0-81769a595d8a>
2005-12-31 07:53:44 +0000

    restructuring description

--- trunk/birbal/births/apps/register/views.py
+++ trunk/birbal/births/apps/register/views.py
@@ -4,59 +4,9 @@ from django.utils.httpwrappers import HttpResponse
 from births.apps.register.models.register import *
 import datetime
 from django.core.exceptions import Http404
-
-menu_items = [
-              {'name':'Home','url':'fac/HM/','id':''},
-              {'name':'FAQ','url':'fac/FQ/','id':''},
-              {'name':'Documents','url':'fac/DC/','id':''},
-              {'name':'Officials','url':'officials/','id':''},
-              ]
-
-fac_types ={
-    'HM': 'Home',
-    'FQ': 'Faq',
-    'DC': 'Documentation',
-    'AM': 'Amenities'
-    }
+from births.apps.website.views import menu_items,extract
 
 
-        
-
-def extract():
-    """ Gets the advertisements and announcements for the
-        right column
-        """
-    list = advertisements.get_values(fields=['title','id'],
-                              expires__gt=datetime.datetime.now(),
-                           order_by=('-date',),
-                              limit=5)
-    return list
-
-
-def index(request):
-    """ Not used
-        """
-    t = template_loader.get_template('register/index')
-    c = Context(request,
-                {'ads':extract(),
-                 'mn': menu_items,
-                 })
-    
-    return HttpResponse(t.render(c))
-
-def fac(request,type):
-    """ View for display of the documentation
-        generates all pages on the fly
-        """
-    list = facilitys.get_list(type__exact=type,)
-    t = template_loader.get_template('register/fac')
-    c = Context(request,
-                {'list':list,'tp':type,
-                 'ads':extract(),
-                 'mn': menu_items,
-                 })
-    
-    return HttpResponse(t.render(c))
 
 def dutyroster(request):
     """ Shows attendance status for employees
--- trunk/birbal/births/urls.py
+++ trunk/birbal/births/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('',
 
     # Uncomment this for admin:
      (r'^births/admin/', include('django.contrib.admin.urls.admin')),
-    (r'^births/$', 'births.apps.register.views.index'),
+    (r'^births/$', 'births.apps.website.views.index'),
     (r'^births/officials/$', 'births.apps.register.views.dutyroster'),
-    (r'^births/ads/(?P<id>\d+)/$', 'births.apps.register.views.ads'),
-    (r'^births/fac/(?P<type>\w+)/$', 'births.apps.register.views.fac'),
+    (r'^births/ads/(?P<id>\d+)/$', 'births.apps.website.views.ads'),
+    (r'^births/fac/(?P<type>\w+)/$', 'births.apps.website.views.fac'),
 )
