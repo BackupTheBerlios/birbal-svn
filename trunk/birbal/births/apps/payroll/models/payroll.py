8163c425173e6a182175c6ce86edbb720ac25e6b
lawgon <lawgon@d3dd426a-df03-0410-81c0-81769a595d8a>
2005-12-31 14:38:33 +0000

    last commit for the year

--- trunk/birbal/births/apps/payroll/models/payroll.py
+++ trunk/birbal/births/apps/payroll/models/payroll.py
@@ -43,7 +43,8 @@ class Person(meta.Model):
         set up we could remove the status field
         """
     name = meta.CharField(_("Name"),maxlength=50,unique=True)
-    rank = meta.ForeignKey(Official)
+    rank = meta.ForeignKey(Official,
+                           verbose_name=_("Rank"))
     status = meta.ForeignKey(Status)
     class META:
         ordering = ['rank','name'.lower()]
@@ -55,3 +56,60 @@ class Person(meta.Model):
             %(self.name,self.get_rank(),self.get_status())
 
 #############################################end of persons
+
+#############################################transactions
+# basically the attendance/movement register. Will have to define
+# holidays, earned leave, causal leave, medical leave etc
+
+class Leave(Power):
+    """Types of leave available
+       """
+    name = meta.CharField(_("Leave Type"),maxlength=50,unique=True)
+
+class Leaveallotment(meta.Model):
+    """This gives the amount of leave available for each
+       type of leave for each type of official
+       """
+    year = meta.IntegerField(_("Year"))
+    leave = meta.ForeignKey(Leave,
+                            verbose_name=_("Type of leave"))
+    rank =  meta.ForeignKey(Official,
+                           verbose_name=_("Rank"))
+    monthly = meta.IntegerField(_("Monthly quota"))
+    yearly = meta.IntegerField(_("Yearly quota"))
+    carriedforward = meta.BooleanField(_("Can be carried forward"),
+                                       default=True)
+    encashed = meta.BooleanField(_("Can be encashed"),
+                                       default=True)
+
+    class META:
+        
+        admin = meta.Admin(
+        list_display = ('year','leave','rank',),
+            search_fields = ['leave','rank'],)
+        unique_together = (("year","leave","rank"),)
+    def __repr__(self):
+            return "%s %s %s"\
+            %(self.year,self.get_rank(),self.get_leave())
+
+class Holiday(meta.Model):
+    """This gives the holidays announced for each year
+       """
+    name = meta.CharField(_("Holiday Name"),maxlength=50,unique=True)
+    year = meta.IntegerField(_("Year"))
+    date = meta.DateField(_("Date"))
+    general = meta.BooleanField(_("General or Restricted"),
+                                       default=True)
+
+    class META:
+        ordering = ['-date','name'.lower()]
+        admin = meta.Admin(
+        list_display = ('year','name','date',),
+            search_fields = ['leave','rank'],)
+    def __repr__(self):
+            return "%s %s %s"\
+            %(self.year,self.date,self.name)
+    
+    
+    
+    
