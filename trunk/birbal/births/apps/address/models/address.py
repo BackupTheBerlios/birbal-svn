b6847a3c01d66e6d87afdab2c1f1c139c66ff035
lawgon <lawgon@d3dd426a-df03-0410-81c0-81769a595d8a>
2005-12-31 08:31:31 +0000

    restructuring into something that looks sane

--- trunk/birbal/births/apps/address/models/address.py
+++ trunk/birbal/births/apps/address/models/address.py
@@ -1,3 +1,31 @@
 from django.core import meta
 
-# Create your models here.
+# All address related data here. There should be a clickable map
+# to populate the Area and Street tables/fields
+
+class Area(meta.Model):
+    """ Each jurisdiction is divided into areas. Areas can be wards,
+        mohallas, sectors etc
+        """
+    name = meta.CharField(_("Area"),maxlength=50,unique=True)
+    class META:
+        admin = meta.Admin(
+        list_display = ('name',),
+            search_fields = ['name'],)
+    def __repr__(self):
+            return "%s" %(self.name)
+
+
+class Street(meta.Model):
+    """ Streets may be streets or colonies or slums or whatever
+        they have to be unique in an area
+        """
+    name = meta.CharField(_("Street"),maxlength=50)
+    area = meta.ForeignKey(Area)
+    class META:
+        unique_together = (("name","area"),)
+        admin = meta.Admin(
+        list_display = ('name','area',),
+            search_fields = ['name'],)
+    def __repr__(self):
+            return _("Street: %s Area %s") %(self.name,self.get_area)
--- trunk/birbal/births/apps/register/models/register.py
+++ trunk/birbal/births/apps/register/models/register.py
@@ -1,5 +1,6 @@
 from django.core import meta
 from births.apps.payroll.models.payroll import *
+from births.apps.address.models.address import *
 
 
 #######################################################################
@@ -12,32 +13,7 @@ sexes = (
 
 # master tables for actual data
 
-class Area(meta.Model):
-    """ Each jurisdiction is divided into areas. Areas can be wards,
-        mohallas, sectors etc
-        """
-    name = meta.CharField(_("Area"),maxlength=50,unique=True)
-    class META:
-        admin = meta.Admin(
-        list_display = ('name',),
-            search_fields = ['name'],)
-    def __repr__(self):
-            return "%s" %(self.name)
 
-
-class Street(meta.Model):
-    """ Streets may be streets or colonies or slums or whatever
-        they have to be unique in an area
-        """
-    name = meta.CharField(_("Street"),maxlength=50)
-    area = meta.ForeignKey(Area)
-    class META:
-        unique_together = (("name","area"),)
-        admin = meta.Admin(
-        list_display = ('name','area',),
-            search_fields = ['name'],)
-    def __repr__(self):
-            return _("Street: %s Area %s") %(self.name,self.get_area)
 
 class Deathplace(Area):
     """ Type of death place - at home, hospital, on the street ...
